74480b709f8ac0a9ae6bcd38175922a8f6e93f74
Frederik Hess <cph-fp103@cphbusiness.dk>
2023-02-21 11:23:32 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import List from "./components/List";
+import AddToList from "./components/AddToList";
+
+export interface IState {
+  people: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string;
+  }[];
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "Frederik",
+      age: 26,
+      url: "https://lh3.googleusercontent.com/pw/AMWts8Aw6wPpRfAkJIW2DI4aZWlI2Pzlttaz_Lg_8VmHmg6RR0t10qiOb6ArJxwB8Fq-f6BGHjprEJBqe45HGwqGt7Jqz696Vde44AEXchVp86oZg76b61ljUkFZk6AO3PxAKe3H8D6bEz8kfpvfUPMqnsvr9w=w1406-h937-s-no?authuser=0",
+      note: "I am a monkey",
+    },
+    {
+      name: "Casper",
+      age: 27,
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg/220px-Bonnet_macaque_%28Macaca_radiata%29_Photograph_By_Shantanu_Kuveskar.jpg",
+    },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monkeys invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
